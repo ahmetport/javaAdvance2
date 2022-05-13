6c4f859b864ecc751877b52ae9a05330efccf51b
ahmet portakal <ahmettportakal27@gmail.com>
2022-05-13 13:59:28 +0300

    13.05.2022

---
+++ src/Projeler/C09.java
@@ -0,0 +1,37 @@
+package Projeler;
+
+import java.util.Scanner;
+
+public class C09 {
+
+
+    /*
+    Bakıye (başlangıç bakıyesi 1000 tl) öğrenme, para yatırma ve cekme, çıkış işlemlerinin yapıldıgı bie ATM app. code create ediniz
+     */
+    public static void main(String[] args) {
+        System.out.println("**** atm ye hoşgeldiniz****");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bakiye sorgulama:1\n para çekme:2\n para yatırma:3\n çıkış:4\n");
+        int işlem = scan.nextInt();
+        int bakıye = 1000;
+
+        switch (işlem) {
+            case 1:
+                System.out.println("mevcut bakıyeniz: " + bakıye);
+                break;
+            case 2:
+                System.out.println("çekeceginiz mıktarıgiriniz");
+                int çekılenmıktar = scan.nextInt();
+                if (çekılenmıktar > bakıye) {
+                    System.out.println("yuru gıt lan işine");
+
+                } else {
+                    bakıye -= çekılenmıktar;
+                    System.out.println("bakıyenızın guncel halı:" + bakıye);
+                }
+                break;
+
+
+        }
+    }
+}
