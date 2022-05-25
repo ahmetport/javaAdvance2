ea4a01a9f61a50da3216ce5285a4f302324981df
ahmet portakal <ahmettportakal27@gmail.com>
2022-05-25 22:23:41 +0300

    25.05.2022

---
+++ src/Interwıevsorular/C08.java
@@ -0,0 +1,41 @@
+package Interwıevsorular;
+
+import java.util.Scanner;
+
+public class C08 {
+
+
+
+    public static void main(String[] args) {
+        //kullanıcıdan toplamak istedigi sayıları alın ve kullanıcı sıfır a basıncaya kadar devam edin
+        //kullanıcı sıfıra bastıgında girdigi tüm sayıların toplamını yazdırın
+        Scanner scan=new Scanner(System.in);
+        int sayı=0;
+        int toplam=0;
+        /*
+        for (int i = 1; i <1000 ; i++) {//sayı afaki girildi
+            System.out.println("lütfen bir sayı giriniz");
+            sayı=scan.nextInt();
+
+            if(sayı==0){
+                break;
+            }else{
+                toplam+=sayı;
+            }
+        }
+
+         */
+        System.out.println(toplam);
+
+        sayı=1;//0 atarsak direk ölü dogar o yuzden bir atadık çünkü aşagıda sayı!=0 oldugu için
+        toplam=0;
+        while (sayı!=0){//sayı 0 eşit olmadıgı muddetçe toplayacak
+            System.out.println("lütfen bir sayı giriniz");
+            sayı=scan.nextInt();
+            toplam+=sayı;//sayıyı toplayarak gidecek kullanıcının girdigi
+        }
+        System.out.println(toplam);
+    }
+}
+
+
